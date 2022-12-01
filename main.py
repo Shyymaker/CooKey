56b3b22d37d78b90e70c94b631b37224f5520700
Shyymaker <117361463+Shyymaker@users.noreply.github.com>
2022-12-01 15:46:48 +0200

    Added page - contact

--- main.py
+++ main.py
@@ -98,6 +98,10 @@ def addPost():
 def about():
     return render_template('about_us.html', menu=dbase.getMenu(), title="Про нас")
 
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', menu=dbase.getMenu(), title="Зворотій зв'язок")
+
 @app.route("/post/<int:id_post>")
 @login_required
 def showPost(id_post):
